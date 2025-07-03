b059e10c3cea2bf190f634a5d0d1b37c4a4d4317
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-07-03 14:48:37 +0300

    Add allClear and clearEntry button functionalities, fix operate function by changing from string to number

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* =========================== Global Values =========================== */
+/* =========================== Global Variables =========================== */
 
 // ----- Number Buttons -----
 const one = document.querySelector(".number_1");
@@ -18,6 +18,8 @@ const addition = document.querySelector("#addition");
 const multiplication = document.querySelector("#multiplication");
 const division = document.querySelector("#division");
 const equals = document.querySelector("#equals");
+const clearEntry = document.querySelector("#clearEntry");
+const allClear = document.querySelector("#allClear");
 
 // ----- Display -----
 const display = document.querySelector(".display");
@@ -30,7 +32,109 @@ let operator = '';
 let result = '';
 
 
-/* =========================== Changing Operators =========================== */
+/* =========================== Setting Numbers =========================== */
+one.addEventListener("click", () => {
+    if (operator == '') {
+        num1 += '1';
+        displayNum(num1);
+    } else {
+        num2 += '1';
+        displayNum(num2);
+    }
+});
+
+two.addEventListener("click", () => {
+    if (operator == '') {
+        num1 += '2';
+        displayNum(num1);
+    } else {
+        num2 += '2';
+        displayNum(num2);
+    }
+});
+
+three.addEventListener("click", () => {
+    if (operator == '') {
+        num1 += '3';
+        displayNum(num1);
+    } else {
+        num2 += '3';
+        displayNum(num2);
+    }
+});
+
+four.addEventListener("click", () => {
+    if (operator == '') {
+        num1 += '4';
+        displayNum(num1);
+    } else {
+        num2 += '4';
+        displayNum(num2);
+    }
+});
+
+five.addEventListener("click", () => {
+    if (operator == '') {
+        num1 += '5';
+        displayNum(num1);
+    } else {
+        num2 += '5';
+        displayNum(num2);
+    }
+});
+
+six.addEventListener("click", () => {
+    if (operator == '') {
+        num1 += '6';
+        displayNum(num1);
+    } else {
+        num2 += '6';
+        displayNum(num2);
+    }
+});
+
+seven.addEventListener("click", () => {
+    if (operator == '') {
+        num1 += '7';
+        displayNum(num1);
+    } else {
+        num2 += '7';
+        displayNum(num2);
+    }
+});
+
+eight.addEventListener("click", () => {
+    if (operator == '') {
+        num1 += '8';
+        displayNum(num1);
+    } else {
+        num2 += '8';
+        displayNum(num2);
+    }
+});
+
+nine.addEventListener("click", () => {
+    if (operator == '') {
+        num1 += '9';
+        displayNum(num1);
+    } else {
+        num2 += '9';
+        displayNum(num2);
+    }
+});
+
+zero.addEventListener("click", () => {
+    if (operator == '') {
+        num1 += '0';
+        displayNum(num1);
+    } else {
+        num2 += '0';
+        displayNum(num2);
+    }
+});
+
+
+/* =========================== Functional Operators =========================== */
 
 addition.addEventListener("click", () => {
     operator = '+';
@@ -52,29 +156,53 @@ equals.addEventListener("click", () => {
     operate(operator, num1, num2);
 });
 
+allClear.addEventListener("click", () => {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    result = '';
+    displayNum('');      
+});
+
+clearEntry.addEventListener("click", () => {
+    if (operator == '') {
+        num1 = num1.substring(0, num1.length - 1);
+        displayNum(num1);
+    } else {
+        num2 = num2.substring(0, num2.length - 1);
+        displayNum(num2);
+    }
+});
 
 /* ===========================  Operator Function =========================== */
 
-function operate (operator, num1, num2){
+function operate(operator, num1, num2) {
+    let a = Number(num1);
+    let b = Number(num2);
+    let result;
+
     switch (operator) {
         case '+':
-            result = add(num1, num2);
-            displayNum(result);
+            result = add(a, b);
             break;
         case '-':
-            result = subtract(num1, num2);
-            displayNum(result);
+            result = subtract(a, b);
             break;
         case '/':
-            result = divide(num1, num2);
-            displayNum(result);
+            result = divide(a, b);
             break;
         case '*':
-            result = multiply(num1, num2);
-            displayNum(result);
+            result = multiply(a, b);
             break;
+        default:
+            return;
     }
-} 
+
+    displayNum(result);
+    num1 = result; // store results in num1 so you can just press another operator
+    num2 = '';
+    return num1;
+}
 
 
 /* =========================== Math Functions =========================== */
